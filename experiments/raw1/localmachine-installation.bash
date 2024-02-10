8089a225d7acb3d34aedfb98222396805f50c307
Sohail Siadat <sohale@gmail.com>
2024-02-10 23:31:21 +0000

    Update localmachine-installation.bash

--- experiments/raw1/localmachine-installation.bash
+++ experiments/raw1/localmachine-installation.bash
@@ -7,5 +7,6 @@ terraform -v
 
 # update ( based on [2] ) 
 
-# based on https://docs.digitalocean.com/reference/terraform/getting-started/
-# [2] https://www.terraform.io/downloads.html )
+# [1] Based on: https://docs.digitalocean.com/reference/terraform/getting-started/
+# [2] Upgrade: https://www.terraform.io/downloads.html
+
