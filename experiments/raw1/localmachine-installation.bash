a4336d92730a2884e4ffff95b9b3f0d4eedd3c4f
Sohail Siadat <sohale@gmail.com>
2024-02-10 23:30:52 +0000

    Update localmachine-installation.bash

--- experiments/raw1/localmachine-installation.bash
+++ experiments/raw1/localmachine-installation.bash
@@ -5,5 +5,7 @@
 brew install terraform
 terraform -v
 
+# update ( based on [2] ) 
 
 # based on https://docs.digitalocean.com/reference/terraform/getting-started/
+# [2] https://www.terraform.io/downloads.html )
