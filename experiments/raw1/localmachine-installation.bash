3f8cb8ee23a20236ef906b6c90c52d0b52fb100f
Sohail Siadat <sohale@gmail.com>
2024-02-10 23:34:39 +0000

    Update localmachine-installation.bash

--- experiments/raw1/localmachine-installation.bash
+++ experiments/raw1/localmachine-installation.bash
@@ -1,17 +1,20 @@
 
 # local machine or workstation
 
-# installing on MacOS
+# (deprecated) Installing on MacOS [1]
+
 # brew install terraform
 # terraform -v
 
 # brew uninstall terraform
 # brew autoremove
 
+# new version based on [3]
 brew tap hashicorp/tap
 
 # update ( based on [2] ) 
 
 # [1] Based on: https://docs.digitalocean.com/reference/terraform/getting-started/
 # [2] Upgrade: https://www.terraform.io/downloads.html
+# [3] New instructions: https://developer.hashicorp.com/terraform/install
 
