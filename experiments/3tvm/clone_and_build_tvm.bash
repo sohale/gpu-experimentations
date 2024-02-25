ed23192c21ccf3c2209b1e522a6a876b915c7a5e
Sohail Siadat <sohale@gmail.com>
2024-02-25 23:25:44 +0000

    3tvm: setup script: install prerequisites

--- experiments/3tvm/clone_and_build_tvm.bash
+++ experiments/3tvm/clone_and_build_tvm.bash
@@ -1,5 +1,22 @@
-# If necessary, set up a VPS using https://gist.github.com/sohale/9112b718625e372b1d407f91f2011296
+# If necessary, First set up a VPS using https://gist.github.com/sohale/9112b718625e372b1d407f91f2011296
+
+set -ex
+
+cd ~/gpu-experimentations/experiments
 
 # Installing (building) from source:
 # Following https://tvm.apache.org/docs/install/from_source.html
-git clone --recursive https://github.com/apache/tvm tvm
+
+# skip
+# git clone --recursive https://github.com/apache/tvm tvm
+
+
+# sudo apt-get update
+sudo apt-get install -y python3 python3-dev python3-setuptools gcc libtinfo-dev zlib1g-dev build-essential cmake libedit-dev libxml2-dev
+
+cd ~/gpu-experimentations/experiments/tvm
+
+mkdir build
+cd build
+
+cp ../cmake/config.cmake ../build
