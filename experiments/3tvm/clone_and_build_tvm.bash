7d934c1836145d54fb0ea662eb6339b15948a8ed
Sohail Siadat <sohale@gmail.com>
2024-02-25 23:10:27 +0000

    Create clone_and_build_tvm.bash

---
+++ experiments/3tvm/clone_and_build_tvm.bash
@@ -0,0 +1,5 @@
+# If necessary, set up a VPS using https://gist.github.com/sohale/9112b718625e372b1d407f91f2011296
+
+# Installing (building) from source:
+# Following https://tvm.apache.org/docs/install/from_source.html
+git clone --recursive https://github.com/apache/tvm tvm
