5e00bd60c480ac615e0aeb6b2a415f41c44bc078
Sohail Siadat <sohale@gmail.com>
2024-03-04 23:14:11 +0000

    why did I forget to push these main terraform statements?

--- experiments/raw1/paperspace_gpu_experiment_terraform.tf
+++ experiments/raw1/paperspace_gpu_experiment_terraform.tf
@@ -59,3 +59,32 @@ terraform {
     }
   }
 }
+
+resource "paperspace_script" "my-startup-script-1" {
+  name = "My Script"
+  description = "a short description"
+  script_text = <<EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+ufw allow 8080
+nohup busybox httpd -f -p 8080 &
+EOF
+  is_enabled = true
+  run_once = false
+}
+
+resource "paperspace_machine" "my-machine-1" {
+  region = "East Coast (NY2)" // optional, defaults to provider region if not specified
+  name = "Terraform Test"
+  machine_type = "C1"
+  size = 50
+  billing_type = "hourly"
+  assign_public_ip = false // optional, remove if you don't want a public ip assigned
+
+  template_id = data.paperspace_template.my-template-1.id
+  user_id = data.paperspace_user.my-user-1.id  // optional, remove to default
+  team_id = data.paperspace_user.my-user-1.team_id
+  script_id = paperspace_script.my-startup-script-1.id
+  shutdown_timeout_in_hours = 1
+  # live_forever = true # enable this to make the machine have no shutdown timeout
+}
