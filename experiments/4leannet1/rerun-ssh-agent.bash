5c6294415e74d694bec4db2bafe1e5e6dd2bcc4c
Sohail Siadat <sohale@gmail.com>
2024-03-17 18:46:54 +0000

    betterment of ssh-agent refresh /rerun script (2): needs to eb done manually

--- experiments/4leannet1/rerun-ssh-agent.bash
+++ experiments/4leannet1/rerun-ssh-agent.bash
@@ -1,12 +1,25 @@
 
 set -e
-# If no "ssh-agent", launch one:
-ps aux | grep -v grep | grep ssh-ag || \
-eval "$(ssh-agent -s)"
 
-ssh-add ~/.ssh/github_rsa
+# Kill "ssh-agent"s if any:
+ps aux | grep -v grep | grep ssh-ag &&
+ps aux | grep -v grep | grep ssh-ag \
+| awk '{print $2}' | xargs kill
+
+#eval "$(ssh-agent -s)"
+
+#echo "slleep 1"
+#sleep 1
+# Why do I need to run this manually?
+#ssh-add ~/.ssh/github_rsa
 
 echo "Checking existing ssh-agents:"
 ps aux | grep -v grep | grep ssh-ag || :
 
+echo "Manually run the folllwing:"
+
+echo 'eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/github_rsa'
+
+echo
 echo "."
