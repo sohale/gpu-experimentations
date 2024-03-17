92d1c2aebe80af487703d529f55aa8be3119f3df
Sohail Siadat <sohale@gmail.com>
2024-03-17 18:39:45 +0000

    betterment of ssh-agent refresh /rerun script

--- experiments/4leannet1/rerun-ssh-agent.bash
+++ experiments/4leannet1/rerun-ssh-agent.bash
@@ -1,5 +1,12 @@
 
-set -ex
-ps aux|grep ssh-ag
-ssh-add ~/.ssh/github_rsa
+set -e
+# If no "ssh-agent", launch one:
+ps aux | grep -v grep | grep ssh-ag || \
 eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/github_rsa
+
+echo "Checking existing ssh-agents:"
+ps aux | grep -v grep | grep ssh-ag || :
+
+echo "."
