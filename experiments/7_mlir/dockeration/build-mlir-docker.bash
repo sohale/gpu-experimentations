bb0c225f4a50fa7aac88b55aa4a306e275046b13
Sohail Siadat <sohale@gmail.com>
2024-05-18 08:01:26 +0000

    MLIR: better Dockerfile structure, enabilng & installing docker builldx / buildkit

--- experiments/7_mlir/dockeration/build-mlir-docker.bash
+++ experiments/7_mlir/dockeration/build-mlir-docker.bash
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-docker build -t mlir-v-tbc   -f Dockerfile .
+# todo: move docker temp folder to the desired location (cloud container's volume)
+set -eux
+
+# docker version
+docker --version
+# Docker version 24.0.5, build 24.0.5-0ubuntu1~22.04.1
+
+# Make sure buildx is installed
+docker buildx version || \
+{
+   mkdir -p ~/.docker/cli-plugins;
+   BUILD_VERSION=$(curl -s https://api.github.com/repos/docker/buildx/releases/latest | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+   echo "BUILD_VERSION: ${BUILD_VERSION}"
+   curl -Lo ~/.docker/cli-plugins/docker-buildx "https://github.com/docker/buildx/releases/download/${BUILD_VERSION}/buildx-${BUILD_VERSION}.linux-amd64"
+   chmod +x ~/.docker/cli-plugins/docker-buildx
+   docker buildx version
+}
+
+# Featuring "SSH Forwarding":
+# DOCKER_BUILDKIT=1 \
+docker  build \
+   --ssh default \
+   \
+   -t mlir-v-tbc  \
+   -f Dockerfile \
+   .
+
+# Also possible
+   #  --secret id=secret1,src=secret1.txt \
+   #  --secret id=secret2,src=secret2.txt \
