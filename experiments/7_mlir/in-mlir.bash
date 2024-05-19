6c7f7275cd8e48524597c44ae61063a49e31bbcd
Sohail Siadat <sohale@gmail.com>
2024-05-19 14:22:08 +0000

    MLIR: verifying mlir comand works, and adding shortcut, and some env fixes

--- experiments/7_mlir/in-mlir.bash
+++ experiments/7_mlir/in-mlir.bash
@@ -15,7 +15,14 @@ export WORK="/home/ephemssss/gpu-experimentations/experiments/7_mlir"
 # for sake of git commands
 export GIT_REPO_ROOT="/home/ephemssss/gpu-experimentations"
 
-#    MLIR_BIN="\mlir" # Docker's ARG BUILD_WORK_DIR, ENV $WORK_DIR
+# these are paths in the host system ^ (URLs from that view)
+
+# MLIR_BINARY_DIR will bt the actual bin (for that version)
+# todo: also can be exported as a label by docker, and into an ENV
+
+#    MLIR_BIN-->MLIR_BASE??="\mlir" # Docker's ARG BUILD_WORK_DIR, ENV $WORK_DIR
+#    MLIR_USER is docker's user
+#     --env MLIR_HOME="/home/myuser" \
 
 #   --volume "/home/ephemssss/novorender/oda-sdk":"/home/ephemssss/novorender/oda-sdk" \
 #    --workdir "$(pwd)" \
@@ -26,14 +33,14 @@ docker run \
     --net=host \
     \
     --env BASE_PATH="${BASE_PATH}" \
-    --env _initial_cwd="$(pwd)" \
+    --env _initial_host_cwd="$(pwd)" \
     --env HOST_HOME="${HOME}" \
     --env GIT_REPO_ROOT="${GIT_REPO_ROOT}" \
     --volume "$GIT_REPO_ROOT":"$GIT_REPO_ROOT" \
     --volume "$WORK":"$WORK" \
     --workdir "${WORK}" \
     \
-    --env MLIR_BIN="\mlir" \
+    --env MLIR_BASE="/mlir" \
     --env MLIR_USER="myuser" \
     \
     ${MLIR_IMAGE_NAME}  \
@@ -47,6 +54,9 @@ docker run \
 
       echo "You are: $HOME"
       nproc
+
+      # It all works undeer this assumption:
+      # todo: assert "/home/${MLIR_USER}" == $HOME
 
       export DEBIAN_FRONTEND=noninteractive
       # not necessary, but useful for debugging & dev
@@ -92,26 +102,36 @@ docker run \
       echo "« Key paths: »"
       # export PATH="$PATH:/opt/msvc/bin/$DESIRED_ARCH"
       # echo "Added to you path: /opt/msvc/bin/$DESIRED_ARCH"
-      echo "Docker's HOME=$HOME"
+      echo "Docker (MLIR)'s HOME=$HOME"
       echo "HOST_HOME=$HOST_HOME"
       echo "GIT_REPO_ROOT=$GIT_REPO_ROOT"
-      echo "_initial_cwd=$_initial_cwd"
+      echo "_initial_host_cwd=$_initial_host_cwd"
 
       echo -n 'realpath ~  : '
       realpath ~
       realpath ~/.bashrc
       ls -alth ~/.bashrc || :
 
+      # /mlir/llvm-project/build/bin/mlir-opt --version
+      # export MLIR_BINARY_DIR=/mlir/llvm-project/build/bin
+      export MLIR_BINARY_DIR=$MLIR_BASE/llvm-project/build/bin
+      $MLIR_BINARY_DIR/mlir-opt --version
+
+      echo "MLIR_BASE=${MLIR_BASE}"
+      echo "MLIR_BINARY_DIR=${MLIR_BINARY_DIR}"
+
+
       echo "« Key commands: »"
-      echo "./scripts/inside_msvc-wine/compile1.bash" >>~/.bash_history
-      echo "./scripts/inside_msvc-wine/compile2.bash" >>~/.bash_history
-      echo "./scripts/inside_msvc-wine/compile3.bash" >>~/.bash_history
+      # echo "./scripts/inside_msvc-wine/compile3.bash" >>~/.bash_history
+      echo "/mlir/llvm-project/build/bin/mlir-opt --version" >>~/.bash_history
+      echo "$MLIR_BINARY_DIR/mlir-opt --version" >>~/.bash_history
+      echo '$MLIR_BINARY_DIR/mlir-opt --version' >>~/.bash_history
+      echo '$MLIR_BINARY_DIR/mlir-opt ' >>~/.bash_history
       cat  ~/.bash_history
 
-      # not effetive
-      #  >> /home/myuser/.bashrc
-      #  >>~/.bashrc
-      cat <<-'__________BASHRC__________' >> /home/myuser/.bashrc
+
+      #  >> /home/myuser/.bashrc  == $HOME/.bashrc ==   ~/.bashrc
+      cat <<-'__________BASHRC__________' >> ~/.bashrc
 
          echo "inside .bashrc    \$\$=$$"
          export PS1='\[\033[01;33m\]𝓜𝓛𝓘𝓡 \[\033[01;36m\]container\[\033[00m\]:\[\033[01;35m\]@\h \[\033[01;34m\]\w\[\033[00m\]\n\[\033[01;32m\]$(whoami) \[\033[00m\] \[\033[01;33m\]$(cut -c1-12 /proc/1/cpuset)\[\033[01;32m\] \$ \[\033[00m\]'
