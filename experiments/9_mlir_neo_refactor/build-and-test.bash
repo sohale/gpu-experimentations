edbedef366bdb51fd92838a29bfcc50ea3e1e0a4
Sohail Siadat <sohale@gmail.com>
2025-01-24 19:15:09 +0000

    build script build folder

--- experiments/9_mlir_neo_refactor/build-and-test.bash
+++ experiments/9_mlir_neo_refactor/build-and-test.bash
@@ -3,30 +3,34 @@
 set -ex
 
 export PROGNAME=loop_vector_computation
+export BUILD=build
+
+# Build directory
+mkdir -p $BUILD
 
 # Assemble the LLVM IR Code
 # llvm-as loop_vector_computation.llvm
 # llvm-as loop_vector_computation.llvm  -o loop_vector_computation.bc
-llvm-as $PROGNAME.ll  -o $PROGNAME.bc
+llvm-as $PROGNAME.ll  -o $BUILD/$PROGNAME.bc
 
 # Generate Native Assembly
 # llc loop_vector_computation.bc -o loop_vector_computation.s
 # nit used, but to have an updated *.s file
-llc $PROGNAME.bc -o $PROGNAME.s
+llc $BUILD/$PROGNAME.bc -o $BUILD/$PROGNAME.s
 # no -g
 #
 # obj: object file (machine code)
 # asm: (assembly code)
 # null (no output)
-llc -filetype=obj -O0 $PROGNAME.bc -o $PROGNAME.o
+llc -filetype=obj -O0 $BUILD/$PROGNAME.bc -o $BUILD/$PROGNAME.o
 
 
 # Compile to Executable
 # clang loop_vector_computation.s -o loop_vector_computation.executable -lm
 # clang $PROGNAME.s -o $PROGNAME.executable -lm
-clang -g $PROGNAME.o -o $PROGNAME
+clang -g $BUILD/$PROGNAME.o -o $BUILD/$PROGNAME
 
 # ./$PROGNAME.executable
 # Segmentation fault (core dumped)
 # Debug using LLDB
-lldb ./$PROGNAME
+lldb $BUILD/$PROGNAME
