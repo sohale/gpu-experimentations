b21dcd29e80bb7408b895f6d26ed78827231108e
Sohail Siadat <sohale@gmail.com>
2026-01-20 21:44:18 +0000

    now a simple Lean4 proof

--- experiments/16_ll_lean4_revisit/LinlogMy/Basic.lean
+++ experiments/16_ll_lean4_revisit/LinlogMy/Basic.lean
@@ -14,3 +14,16 @@ def hello := "world"
 -- We alredy defined a main in LinlogMy/simply_ll1.lean
 -- def main : IO Unit :=
 --   IO.println s!"linear logoic installed"
+
+
+---------
+
+-- Now, a simple proof:
+
+open Set
+
+example (x : ℝ) (hx : 0 < x) : x ∈ { y : ℝ | 0 ≤ y } := by
+  -- unfold set membership
+  show 0 ≤ x
+  -- strict positivity implies nonnegativity
+  exact le_of_lt hx
