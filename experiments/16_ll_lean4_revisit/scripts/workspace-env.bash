44e0fd45d039cc9b049d6699042727e63f49797a
Sohail Siadat <sohale@gmail.com>
2026-01-24 17:33:22 +0000

    correcting the vscode terminl env settings

--- experiments/16_ll_lean4_revisit/scripts/workspace-env.bash
+++ experiments/16_ll_lean4_revisit/scripts/workspace-env.bash
@@ -1,6 +1,16 @@
 
 set -x
-# cd dataneura/gpu-experimentations/experiments/16_ll_lean4_revisit$
-echo  "workspace-env.bash"
+echo "Details:"
+echo  "doin: workspace-env.bash arg: $1"
+uname -a
+echo "TERM_PROGRAM=$TERM_PROGRAM"
+echo "TERM_PROGRAM_VERSION=$TERM_PROGRAM_VERSION"
+
+set -x
+cd dataneura/gpu-experimentations/experiments/16_ll_lean4_revisit$
 pwd
-exec bash
+
+# The terminal will not stay if I don't add the following command:
+# The `--noprofile` `--norc` needs to be both here and in $MONO_REPO_ROOT/experiments/16_ll_lean4_revisit/.vscode/settings.json
+# Will reset the `set -ex` etc
+exec bash --noprofile --norc
