24c3f073dcd2a542548a8f0127112ce788ae21ae
Sohail Siadat <sohale@gmail.com>
2026-02-04 18:55:14 +0000

    a script to build the ,workspace from yaml

---
+++ experiments/21_cuda_another_warmup_exercise/scripts/dev.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eux
+
+export MONOREPO_ROOT="$HOME/gpu-experimentations"
+
+# paths
+# VSCODE_WSDIR
+WS="$MONOREPO_ROOT/experiments/21_cuda_another_warmup_exercise"
+WSFILE=$WS/exper21.generated.code-workspace
+
+cd "$WS"
+
+# installation
+: || {
+  # clang++ installation as of 21.1.7
+  # CUDA instation: as of: 13-1
+  :
+}
+
+# each time
+cat scripts/code-workspace.yaml \
+  | yq -o=json eval \
+    > $WSFILE
+
+# For documentation. Not automating.
+echo "On vscode, click on $(realpath --relative-to="$WS" "$WSFILE")"
