2d9cb77004d5292f69c5d5ff5f95fcf0eb2a1847
Sohail Siadat <sohale@gmail.com>
2026-02-04 22:33:30 +0000

    lint/reformat c++ source code

--- experiments/21_cuda_another_warmup_exercise/cpp_test1.cpp
+++ experiments/21_cuda_another_warmup_exercise/cpp_test1.cpp
@@ -1,5 +1,5 @@
-// A funcoitn tha ttake stwo ASCII strings, returns true: if stirnga are anagrams
-// state and taste
+// A funcoitn tha ttake stwo ASCII strings, returns true: if stirnga are
+// anagrams state and taste
 
 #include <string>
 #include <unordered_map>
@@ -12,28 +12,28 @@ typedef std::string::value_type char_type;
 typedef unordered_map<char_type, int> shelf_t;
 
 /**
- * true: 
+ * true:
  */
-bool processAChar(shelf_t & shelf, char_type c, bool add_keys, int increment) {
-   typedef  shelf_t::iterator itertype;
-   const std::pair<itertype, bool> keyresult = shelf.find(c);
-   if (key == shelf.end()) {
-      if (add_keys) {
-           // key = std::emplace()
-           // key = shelf.emplace(key);
-           shelf.insert(key, 0);
-           shelf[key] += cincreamtn
-      } else {
-         retur false;
-      }
+bool processAChar(shelf_t &shelf, char_type c, bool add_keys, int increment) {
+  typedef shelf_t::iterator itertype;
+  const std::pair<itertype, bool> keyresult = shelf.find(c);
+  if (key == shelf.end()) {
+    if (add_keys) {
+      // key = std::emplace()
+      // key = shelf.emplace(key);
+      shelf.insert(key, 0);
+      shelf[key] += cincreamtn
+    } else {
+      retur false;
     }
-    return true;
-   }
+  }
+  return true;
+}
 
 bool isAnagram(const string &a, const string &a) {
-   for (int li = 0; li<a.size(); li++ ) {
-      ;
-   }
+  for (int li = 0; li < a.size(); li++) {
+    ;
+  }
 }
 
 /*
